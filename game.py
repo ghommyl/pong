21c8d396e79a2b77cfa9a4574e38aad495fdd962
ghommyl <tommy21153@outlook.com>
2023-08-20 16:10:46 +0800

    paddles controlling added

--- game.py
+++ game.py
@@ -5,34 +5,33 @@ import pygame
 
 from settings import settings
 
-logging.basicConfig(level=logging.DEBUG, format="%(levelname)-10s- %(message)s")
+logging.basicConfig(level=logging.INFO,
+                    format="%(asctime)s - %(levelname)-7s - %(message)s",
+                    datefmt="%Y-%m-%d %H:%M:%S")
 
 
 class Game:
     def __init__(self):
         self.screen_width, self.screen_height = settings["screen_width"], settings["screen_height"]
-        logging.info(f"screen width: {self.screen_width}, screen height: {self.screen_height}")
 
         self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
         
         pygame.display.set_caption("Welcome to Pong Game!")
 
         self.bg_color = tuple(settings["bg_color"])
-        logging.info(f"bg_color: {self.bg_color}")
 
         self.clock = pygame.time.Clock()
 
         self.fps = settings["fps"]
-        logging.info(f"fps: {self.fps}")
 
         self.paddle_width, self.paddle_height = settings["paddle_width"], settings["paddle_height"]
         self.paddle_border_distance_x = settings["paddle_border_distance_x"]
-        self.paddle_left_rect = pygame.Rect(self.paddle_border_distance_x, 
-                                       round(self.screen_height / 2 - self.paddle_height / 2), 
-                                       self.paddle_width, self.paddle_height)
-        self.paddle_right_rect = pygame.Rect(self.screen_width - self.paddle_border_distance_x - self.paddle_width, 
-                                        round(self.screen_height / 2 - self.paddle_height / 2), 
-                                        self.paddle_width, self.paddle_height)
+        self.paddle_left = Paddle(self.paddle_border_distance_x,                                            # left
+                                  round(self.screen_height / 2 - self.paddle_height / 2),                   # top
+                                  self.paddle_width, self.paddle_height)
+        self.paddle_right = Paddle(self.screen_width - self.paddle_border_distance_x - self.paddle_width,   # left
+                                   round(self.screen_height / 2 - self.paddle_height / 2),                  # top
+                                   self.paddle_width, self.paddle_height)
     
     def run(self):
         while True:
@@ -46,16 +45,75 @@ class Game:
                 case pygame.QUIT:
                     logging.info("exiting game")
                     sys.exit()
+                
+                case pygame.KEYDOWN:
+                    self._check_keydown_events(event)
+                case pygame.KEYUP:
+                    self._check_keyup_events(event)
     
     def _update_screen(self):
+        self.paddle_left.update()
+        self.paddle_right.update()
+
         self.screen.fill(self.bg_color)
 
-        # blit something on the screen
-        pygame.draw.rect(self.screen, settings["paddle_color"], self.paddle_left_rect)
-        pygame.draw.rect(self.screen, settings["paddle_color"], self.paddle_right_rect)
+        pygame.draw.rect(self.screen, settings["paddle_color"], self.paddle_left)
+        pygame.draw.rect(self.screen, settings["paddle_color"], self.paddle_right)
 
         self.clock.tick(self.fps)
         pygame.display.flip()
     
     def _check_collisions(self):
         pass
+
+    def _check_keydown_events(self, event):
+        match event.key:
+            case pygame.K_w:
+                logging.debug("left paddle moving up")
+                self.paddle_left.moving_up = True
+            case pygame.K_s:
+                logging.debug("left paddle moving down")
+                self.paddle_left.moving_down = True
+            case pygame.K_UP:
+                logging.debug("right paddle moving up")
+                self.paddle_right.moving_up = True
+            case pygame.K_DOWN:
+                logging.debug("right paddle moving down")
+                self.paddle_right.moving_down = True
+            
+            case pygame.K_q:
+                logging.info("exiting game")
+                sys.exit()
+
+    def _check_keyup_events(self, event):
+        match event.key:
+            case pygame.K_w:
+                logging.debug("left paddle stopped")
+                self.paddle_left.moving_up = False
+            case pygame.K_s:
+                logging.debug("left paddle stopped")
+                self.paddle_left.moving_down = False
+            case pygame.K_UP:
+                logging.debug("right paddle stopped")
+                self.paddle_right.moving_up = False
+            case pygame.K_DOWN:
+                logging.debug("right paddle stopped")
+                self.paddle_right.moving_down = False
+
+
+class Paddle(pygame.Rect):
+    def __init__(self, left, top, width, height):
+        super().__init__(left, top, width, height)
+        self.moving_up = self.moving_down = False
+        self.speed = settings["paddle_speed"]
+        self.screen_width, self.screen_height = settings["screen_width"], settings["screen_height"]
+
+    def update(self):
+        if self.moving_up:
+            self.top -= self.speed
+        if self.moving_down:
+            self.top += self.speed
+        if self.top < 0:
+            self.top = 0
+        if self.bottom > self.screen_height:
+            self.bottom = self.screen_height
--- settings.py
+++ settings.py
@@ -1,7 +1,9 @@
 import json
 import logging
 
-logging.basicConfig(level=logging.DEBUG, format="%(levelname)-10s- %(message)s")
+logging.basicConfig(level=logging.INFO,
+                    format="%(asctime)s - %(levelname)-7s - %(message)s",
+                    datefmt="%Y-%m-%d %H:%M:%S")
 
 filename = "settings.json"
 file = open(filename)
@@ -9,4 +11,5 @@ settings = json.load(file)
 file.close()
 
 logging.info("settings.json loaded")
-logging.debug(f"settings.json: {settings}")
+for key, value in settings.items():
+    logging.debug(f"{key}: {value}")
